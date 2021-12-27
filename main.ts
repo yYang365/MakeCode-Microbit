b05975d216c725c303272d16d366c323d7610e69
yYang365 <yyang365@outlook.com>
2021-12-28 04:03:31 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
